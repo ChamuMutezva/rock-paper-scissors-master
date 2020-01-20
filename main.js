7a497d4dc40030238623967f00ddea26ce935527
Lucas <gomez-lucas@live.com.ar>
2020-01-19 23:31:44 -0300

    layout modifications

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const closeBtn = document.querySelector(".fa-times");
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".modal-container");
 const rules = document.querySelector(".rules");
 const choices = document.querySelector(".choices");
 console.log(choices);
+
 closeBtn.addEventListener("click", function(){
     console.log("hello world");
     modal.style.display = "none";
 })
 rules.addEventListener("click", function(){  
-    modal.style.display = "block";
+    modal.style.display = "flex";
 }, true)
+
 choices.addEventListener("click", function(evt){
     console.log(evt.target);
 })
