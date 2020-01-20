24f8650ff419a204d8a250925a6e9487e523d858
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-01-20 20:54:39 +0200

    deciding win, loss and draw

--- main.js
+++ main.js
@@ -13,17 +13,84 @@ rules.addEventListener("click", function () {
 }, true)
 
 choices.addEventListener("click", function (evt) {
-    
     let targetElement = evt.target;
-    console.log(targetElement);
+    let myChoice = "";
+    let computerSelection = computerChoice();
+   // console.log(targetElement);
     if (targetElement.classList.contains("paper")) {
-        console.log("You have picked paper");
-        alert("You have picked paper")
+       // console.log("You have picked paper");
+        myChoice = "paper";
+        //  alert("You have picked paper")
     } else if (targetElement.classList.contains("rock")) {
-        console.log("You have picked rock");
-        alert("You have picked rock")
+       // console.log("You have picked rock");
+        myChoice = "rock";
+        //  alert("You have picked rock")
     } else {
-        console.log("You have picked scissors");
-        alert("You have picked scissors")
+        myChoice = "scissors";
+       // console.log("You have picked scissors");
+        //  alert("You have picked scissors")
     }
+   // console.log(computerSelection);
+    playGame(myChoice, computerSelection)
 })
+
+computerChoice = () => {
+    const computerChoices = ["rock", "paper", "scissors"];
+    const compAnswer = Math.floor(Math.random() * computerChoices.length);
+    // console.log(computerChoices[compAnswer]);
+    return computerChoices[compAnswer];
+}
+
+playGame = (myChoice, computerChoice) => {
+    let msg = "game starts...";
+   // console.log(msg);
+    console.log("My choice is " + myChoice);
+    console.log("Computer choice is " + computerChoice);
+     switch (myChoice) {
+        case "rock":
+            if (computerChoice == "rock") {
+                console.log("It's a draw");
+                msg = "It's a draw";
+            } else if (computerChoice == "paper") {
+                console.log("Computer wins");
+                msg = "Computer wins";
+            } else {
+                console.log("I win");
+                msg = "I win";
+            }
+            return msg;
+            break;
+
+        case "paper": {
+            if (computerChoice == "paper") {
+                console.log("It's a draw");
+                msg = "It's a draw";
+            } else if (computerChoice == "scissors") {
+                console.log("Computer wins");
+                msg = "Computer wins";
+            } else {
+                console.log("I win");
+                msg = "I win";
+            }
+            return msg;
+            break;
+        }
+
+        case "scissors": {
+            if (computerChoice == "scissors") {
+                console.log("It's a draw");
+                msg = "It's a draw";
+            } else if (computerChoice == "rock") {
+                console.log("Computer wins");
+                msg = "Computer wins";
+            } else {
+                console.log("I win");
+                msg = "I win";
+            }
+            return msg;
+            break;
+        }
+
+    } 
+   // console.log(msg);
+}
