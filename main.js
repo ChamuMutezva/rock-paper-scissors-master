87679f863cf0811b0a1e7acdb4665890bef522dd
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-01-20 06:21:04 +0200

    player choice implementation

--- main.js
+++ main.js
@@ -4,14 +4,23 @@ const rules = document.querySelector(".rules");
 const choices = document.querySelector(".choices");
 console.log(choices);
 
-closeBtn.addEventListener("click", function(){
+closeBtn.addEventListener("click", function () {
     console.log("hello world");
     modal.style.display = "none";
 })
-rules.addEventListener("click", function(){  
+rules.addEventListener("click", function () {
     modal.style.display = "flex";
 }, true)
 
-choices.addEventListener("click", function(evt){
-    console.log(evt.target);
+choices.addEventListener("click", function (evt) {
+    
+    let targetElement = evt.target;
+    console.log(targetElement);
+    if (targetElement.classList.contains("paper")) {
+        console.log("You have picked paper");
+    } else if (targetElement.classList.contains("rock")) {
+        console.log("You have picked rock");
+    } else {
+        console.log("You have picked scissors");
+    }
 })
